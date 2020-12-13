6622a655f7f33d788aa547320781277d51bbc11c
hthunder <Nosov.yura.web@gmail.com>
2020-12-14 01:11:39 +0300

    Добавляем шаблонизатор и пока маршрутизацию в лоб

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
-app.use(express.static('public/html'));
+
+app.use('/index-admin', (req, res) => {
+	res.render('index-admin.hbs');
+});
+
+app.use('/index', (req, res) => {
+	res.render('index.hbs');
+});
+
+app.use('/about-admin', (req, res) => {
+  res.render('about-admin.hbs');
+});
+
+app.use('/about', (req, res) => {
+  res.render('about.hbs');
+});
+
+app.use('/articles-admin', (req, res) => {
+  res.render('articles-admin.hbs');
+});
+
+app.use('/articles', (req, res) => {
+  res.render('articles.hbs');
+});
+
+app.use('/my-page', (req, res) => {
+  res.render('my-page.hbs');
+});
+
+app.use('/topic', (req, res) => {
+  res.render('topic.hbs');
+});
 
 const PORT = process.env.PORT || 5000;
 
