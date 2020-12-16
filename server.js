b857fea4da99eabee1509c513fc8735f91d1546c
hthunder <Nosov.yura.web@gmail.com>
2020-12-16 22:35:48 +0300

    мелкие правки

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const hbs = require('hbs');
+const exphbs = require('express-handlebars');
+const blogRoutes = require('./routes/blog');
 
 const app = express();
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
 
-// app.use(express.static('public'));
-// app.set('view engine', 'hbs');
-// hbs.registerPartials(__dirname + '/views/partials');
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
+app.use(blogRoutes);
+app.use(express.static('public'));
 
-// app.get('/about', (req, res) => {
-//   res.render('about');
-// });
-
-// app.get('/articles', (req, res) => {
-//   res.render('articles');
-// });
-
-// app.get('/my-page', (req, res) => {
-//   res.render('my-page');
-// });
-
-// app.get('/topic', (req, res) => {
-//   res.render('topic');
-// });
 const PORT = process.env.PORT || 5000;
 const username = process.env.DB_ADMIN_USERNAME;
 const password = process.env.DB_ADMIN_PASSWORD;
@@ -37,8 +26,8 @@ async function start() {
     await mongoose.connect(
       `mongodb+srv://${username}:${password}@cluster0.m6k5m.mongodb.net/blog`,
       {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         useFindAndModify: false
       }
     );
