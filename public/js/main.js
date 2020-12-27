27b38554f5365407ab66608b8c4b2e2333082c65
hthunder <Nosov.yura.web@gmail.com>
2020-12-28 01:14:37 +0300

    премердж коммит

--- public/js/main.js
+++ public/js/main.js
@@ -1,74 +1,72 @@
-$('.log-in').on('click', function() {
-    $('.pop-up__login').slideToggle()
-    $('.overlay').fadeIn()
-})
+$('.log-in').on('click', function () {
+  $('.pop-up__login').slideToggle();
+  $('.overlay').fadeIn();
+});
 
-$('.cansel-login').on('click', function() {
-    $('.pop-up__login').slideToggle()
-    $('.overlay').fadeOut()
-})
+$('.cansel-login').on('click', function () {
+  $('.pop-up__login').slideToggle();
+  $('.overlay').fadeOut();
+});
 
-$('.cansel-login').on('click', function(e) {
-    e.preventDefault()
-})
+$('.cansel-login').on('click', function (e) {
+  e.preventDefault();
+});
 
-$('.overlay').on('click', function(){
-    $('.pop-up__login').fadeOut()
-    $('.overlay').fadeOut()
-})
+$('.overlay').on('click', function () {
+  $('.pop-up__login').fadeOut();
+  $('.overlay').fadeOut();
+});
 
-$('.sign-up').on('click', function(){
-    $('.pop-up__signup').slideToggle()
-    $('.overlay').fadeIn()
-})
+$('.sign-up').on('click', function () {
+  $('.pop-up__signup').slideToggle();
+  $('.overlay').fadeIn();
+});
 
-$('.cansel-signup').on('click', function() {
-    $('.pop-up__signup').slideToggle()
-    $('.overlay').fadeOut()
-})
+$('.cansel-signup').on('click', function () {
+  $('.pop-up__signup').slideToggle();
+  $('.overlay').fadeOut();
+});
 
-$('.cansel-signup').on('click', function(e) {
-    e.preventDefault()
-})
+$('.cansel-signup').on('click', function (e) {
+  e.preventDefault();
+});
 
-$('.overlay').on('click', function(){
-    $('.pop-up__signup').fadeOut()
-    $('.overlay').fadeOut()
-})
-
+$('.overlay').on('click', function () {
+  $('.pop-up__signup').fadeOut();
+  $('.overlay').fadeOut();
+});
 
 /*Не закрываются выпадающие элементы при повторном клике*/
 
 let burger = document.querySelector('.mobile-menu');
 let mbMenu = document.querySelector('.menu-list');
 
-
-burger.onclick = function(){
-    if (mbMenu.style.display = 'none'){
-        mbMenu.style.display = 'block'
-    } else {
-        mbMenu.classList.add('.hide');
-    }
-}
+burger.onclick = function () {
+  if (mbMenu.classList.contains('open')) {
+    mbMenu.classList.remove('open');
+  } else {
+    mbMenu.classList.add('open');
+  }
+};
 
 let search = document.querySelector('.search-icon');
 let searchField = document.querySelector('.search-wrap');
 
-search.onclick = function(){
-    if (searchField.style.display = 'none'){
-        searchField.style.display = 'block'
-    } else {
-        searchField.style.display = 'none';
-    }
-}
+search.onclick = function () {
+  if (searchField.classList.contains('open')) {
+    searchField.classList.remove('open');
+  } else {
+    searchField.classList.add('open');
+  }
+};
 
 let rubrickBtn = document.querySelector('.mobile-rubrics-btn-wrap');
 let rubricks = document.querySelector('.rubrics-mbl-container');
 
-rubrickBtn.onclick = function(){
-    if (rubricks.style.display = 'none'){
-        rubricks.style.display = 'flex'
-    } else {
-        rubricks.style.display = 'none';
-    }
-}
+rubrickBtn.onclick = function () {
+  if (rubricks.classList.contains('open-rubric')) {
+    rubricks.classList.remove('open-rubric');
+  } else {
+    rubricks.classList.add('open-rubric');
+  }
+};
