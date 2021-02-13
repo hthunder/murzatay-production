0ff3762f60eab68986e59df46759e3fb76b02eae
Yuriy Nosov <Nosov.yura.web@gmail.com>
2021-02-13 16:13:00 +0000

    исправляем ошибку с неубирающимся попапом

--- public/js/main.js
+++ public/js/main.js
@@ -2,79 +2,61 @@ import { addFavourite } from './addFavourite.js';
 import { sendForm } from './sendFormData.js';
 
 $('.log-in').on('click', function () {
-  $('.pop-up__login').slideToggle();
-  $('.overlay').fadeIn();
+    $('.pop-up__login').slideToggle();
+    $('.overlay').fadeIn();
 });
 
 $('.cansel-login').on('click', function () {
-  $('.pop-up__login').slideToggle();
-  $('.overlay').fadeOut();
+    $('.pop-up__login').slideToggle();
+    $('.overlay').fadeOut();
 });
 
 $('.cansel-login').on('click', function (e) {
-  e.preventDefault();
+    e.preventDefault();
 });
 
 $('.overlay').on('click', function () {
-  $('.pop-up__login').fadeOut();
-  $('.overlay').fadeOut();
+    $('.pop-up__login').fadeOut();
+    $('.overlay').fadeOut();
 });
 
 $('.sign-up').on('click', function () {
-  $('.pop-up__signup').slideToggle();
-  $('.overlay').fadeIn();
+    $('.pop-up__signup').slideToggle();
+    $('.overlay').fadeIn();
 });
 
 $('.cansel-signup').on('click', function () {
-  $('.pop-up__signup').slideToggle();
-  $('.overlay').fadeOut();
+    $('.pop-up__signup').slideToggle();
+    $('.overlay').fadeOut();
 });
 
 $('.cansel-signup').on('click', function (e) {
-  e.preventDefault();
+    e.preventDefault();
 });
 
 $('.overlay').on('click', function () {
-  $('.pop-up__signup').fadeOut();
-  $('.overlay').fadeOut();
+    $('.pop-up__signup').fadeOut();
+    $('.overlay').fadeOut();
 });
 
 $('.log-in-mbl').on('click', function () {
-  $('.pop-up__login').slideToggle();
-  $('.overlay').fadeIn();
-});
-
-$('.cansel-login').on('click', function () {
-  $('.pop-up__login').slideToggle();
-  $('.overlay').fadeOut();
-});
-
-$('.cansel-login').on('click', function (e) {
-  e.preventDefault();
+    $('.pop-up__login').slideToggle();
+    $('.overlay').fadeIn();
 });
 
 $('.overlay').on('click', function () {
-  $('.pop-up__login').fadeOut();
-  $('.overlay').fadeOut();
+    $('.pop-up__login').fadeOut();
+    $('.overlay').fadeOut();
 });
 
 $('.sign-up-mbl').on('click', function () {
-  $('.pop-up__signup').slideToggle();
-  $('.overlay').fadeIn();
-});
-
-$('.cansel-signup').on('click', function () {
-  $('.pop-up__signup').slideToggle();
-  $('.overlay').fadeOut();
-});
-
-$('.cansel-signup').on('click', function (e) {
-  e.preventDefault();
+    $('.pop-up__signup').slideToggle();
+    $('.overlay').fadeIn();
 });
 
 $('.overlay').on('click', function () {
-  $('.pop-up__signup').fadeOut();
-  $('.overlay').fadeOut();
+    $('.pop-up__signup').fadeOut();
+    $('.overlay').fadeOut();
 });
 
 /*Не закрываются выпадающие элементы при повторном клике*/
@@ -83,33 +65,33 @@ let burger = document.querySelector('.mobile-menu');
 let mbMenu = document.querySelector('.menu-list');
 
 burger.onclick = function () {
-  if (mbMenu.classList.contains('open')) {
-    mbMenu.classList.remove('open');
-  } else {
-    mbMenu.classList.add('open');
-  }
+    if (mbMenu.classList.contains('open')) {
+        mbMenu.classList.remove('open');
+    } else {
+        mbMenu.classList.add('open');
+    }
 };
 
 let search = document.querySelector('.search-icon');
 let searchField = document.querySelector('.search-wrap');
 
 search.onclick = function () {
-  if (searchField.classList.contains('open')) {
-    searchField.classList.remove('open');
-  } else {
-    searchField.classList.add('open');
-  }
+    if (searchField.classList.contains('open')) {
+        searchField.classList.remove('open');
+    } else {
+        searchField.classList.add('open');
+    }
 };
 
 let rubrickBtn = document.querySelector('.mobile-rubrics-btn-wrap');
 let rubricks = document.querySelector('.rubrics-mbl-container');
 
 rubrickBtn.onclick = function () {
-  if (rubricks.classList.contains('open-rubric')) {
-    rubricks.classList.remove('open-rubric');
-  } else {
-    rubricks.classList.add('open-rubric');
-  }
+    if (rubricks.classList.contains('open-rubric')) {
+        rubricks.classList.remove('open-rubric');
+    } else {
+        rubricks.classList.add('open-rubric');
+    }
 };
 
 addFavourite();
