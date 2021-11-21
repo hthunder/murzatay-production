730e591e7ead7ab5203b1832a78b162309d62bc4
Yuriy Nosov <Nosov.yura.web@gmail.com>
2021-11-21 14:03:00 +0000

    Добавил проверку роли админа перед удалением статьи и исключил для rsync скрытые файлы

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/articles.controller")
 const fileHandling = require("../middlewares/fileHandling")
+const { isAdmin } = require("../middlewares/authJwt")
 
 // only for admins
 router.get("/add", controller.article_create_get)
@@ -14,7 +15,7 @@ router.get("/:slug", controller.article_page)
 
 router.put("/:id", fileHandling, controller.article_edit_put)
 
-router.delete("/:id", controller.article_remove)
+router.delete("/:id", isAdmin, controller.article_remove)
 
 router.post("/add", fileHandling, controller.article_create_post)
 
